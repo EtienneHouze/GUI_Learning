2d8920bfe3e80a0b17c5ea46911929566f0330c3
Unknown <etienne.houze@polytechnique.edu>
2017-07-19 17:29:05 +0200

    Commentnig

--- Python/gui/MainWindow.py
+++ Python/gui/MainWindow.py
@@ -6,7 +6,13 @@ from gui.PostProcFrame import PostProcFrame
 from gui.MeshFrame import MeshFrame
 
 class MainWindow(tk.Frame):
+    """
+    Class describing the main window of the application.
+    """
     def __init__(self, master=None):
+        """
+        Instanciation.
+        """
         tk.Frame.__init__(self,master)
         self.master.rowconfigure(0,weight=1)
         self.master.columnconfigure(0,weight=1)
@@ -16,24 +22,48 @@ class MainWindow(tk.Frame):
 
 
     def open_preproc(self):
+        """
+        Callback for the preprocessing button.
+
+        Returns :
+            Open the preprocessing panel.
+        """
         if self.frame:
             self.frame.destroy()
         self.frame = PreprocFrame(self)
         self.frame.grid(row=1,columnspan=4, pady=10)
 
-    def open_inference(self):
+    def open_training(self):
+        """
+        Callback for the training button.
+
+        Returns :
+            Open the training panel.
+        """
         if self.frame:
             self.frame.destroy()
         self.frame = InferenceFrame(self)
         self.frame.grid(row=1,columnspan=4,pady=10)
 
-    def open_training(self):
+    def open_inference(self):
+        """
+        Callback for the inference button.
+
+        Returns :
+            Open the inference panel.
+        """
         if self.frame:
             self.frame.destroy()
         self.frame = TrainFrame(self)
         self.frame.grid(row=1,columnspan=4,pady=10)
 
     def open_postproc(self):
+        """
+        Callback for the mesh reconstruction button.
+
+        Returns :
+            Open the mesh reconstruction panel.
+        """
         if self.frame:
             self.frame.destroy()
         self.frame = MeshFrame(self)
@@ -41,11 +71,15 @@ class MainWindow(tk.Frame):
 
 
     def create_widgets(self):
+        """
+        Method called to create and place the different buttons of the window
+        """
         self.bouton1 = tk.Button(self,text='Mesh PreProcess',command=self.open_preproc)
         self.bouton2 = tk.Button(self,text='Training',command=self.open_training)
         self.bouton3 = tk.Button(self,text='Inference', command=self.open_inference)
         self.bouton4 = tk.Button(self,text="Mesh PostProcess", command=self.open_postproc)
         self.frame = tk.Frame(self)
+        
         self.bouton1.grid(row=0,column=0)
         self.bouton2.grid(row=0,column=1)
         self.bouton3.grid(row=0,column=2)
